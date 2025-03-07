bc2ec355dd1d81770fe254b739b507e417e259da
KevinBAustin <122644452+KevinBAustin@users.noreply.github.com>
2025-03-07 16:15:45 -0600

    Add files via upload

---
+++ arith/src/arithlang/Printer.java
@@ -0,0 +1,90 @@
+package arithlang;
+
+import java.util.List;
+
+import static arithlang.AST.*;
+
+import arithlang.AST.AddExp;
+import arithlang.AST.DivExp;
+import arithlang.AST.MultExp;
+import arithlang.AST.NegExp;
+import arithlang.AST.NumExp;
+import arithlang.AST.PowExp;
+import arithlang.AST.Program;
+import arithlang.AST.SubExp;
+import arithlang.AST.AsgExp;
+import arithlang.AST.UnitExp;
+
+public class Printer {
+	public void print(Value v) {
+		System.out.println(v.toString());
+	}
+
+	public void print(Exception e) {
+		System.out.println(e.getMessage());
+	}
+	
+	public static class Formatter implements AST.Visitor<String> {
+		
+		public String visit(Program p, Env env) {
+			return (String) p.e().accept(this, env);
+		}
+		
+		public String visit(NumExp e, Env env) {
+			return "" + e.v();
+		}
+		
+		public String visit(AddExp e, Env env) {
+			String result = "(+";
+			for(AST.Exp exp : e.all()) 
+				result += " " + exp.accept(this, env);
+			return result + ")";
+		}		
+		
+		public String visit(SubExp e, Env env) {
+			String result = "(-";
+			for(AST.Exp exp : e.all()) 
+				result += " " + exp.accept(this, env);
+			return result + ")";
+		}
+		
+		public String visit(MultExp e, Env env) {
+			String result = "(*";
+			for(AST.Exp exp : e.all()) 
+				result += " " + exp.accept(this, env);
+			return result + ")";
+		}
+
+		public String visit(DivExp e, Env env) {
+			String result = "(/";
+			for(AST.Exp exp : e.all()) 
+				result += " " + exp.accept(this, env);
+			return result + ")";
+		}
+		
+		public String visit(PowExp e, Env env) {
+			String result = "(^";
+			for(AST.Exp exp : e.all()) 
+				result += " " + exp.accept(this, env);
+			return result + ")";
+		}
+
+		public String visit(NegExp e, Env env) {
+			String result = "(-";
+			result += " " + e.getExp().accept(this, env);
+			return result + ")";
+		}
+
+/**		public String visit(AST.AsgExp d, Env env) {
+			String result = "(define ";
+			result += d.name() + " ";
+			result += d.value_exp().accept(this, env);
+			return result + ")";
+		}
+*/
+		@Override
+		public String visit(UnitExp e, Env env) {
+			return "unit";
+		}
+	}
+}
